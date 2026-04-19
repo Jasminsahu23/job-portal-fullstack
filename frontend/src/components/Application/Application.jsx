f7d59dfbdb36c4e278ec93fa9aa56470c682839a
Jasmin Sahu <jasminsahu2310@gmail.com>
2026-04-19 22:52:02 +0530

    Fix Vercel blank page

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -74,7 +74,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/post",
+        "https://jobportal-backend-zxh3.onrender.com:4000/api/v1/application/post",
         formData,
         {
           withCredentials: true,
--- frontend/src/components/Job/PostJob.jsx
+++ frontend/src/components/Job/PostJob.jsx
@@ -32,7 +32,7 @@ const PostJob = () => {
     }
     await axios
       .post(
-        "http://localhost:4000/api/v1/job/post",
+        "https://jobportal-backend-zxh3.onrender.com:4000/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
